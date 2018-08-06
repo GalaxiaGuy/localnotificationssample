a2902a16dbb76438734e390c4be4d8f6debd0a3f
Oliver Brown <oliver.brown@chelsea-apps.com>
2018-08-06 16:03:53 +0100

    Initial commit.

---
+++ LocalNotificationsSample.Droid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace LocalNotificationsSample.Droid
+{
+    [Activity(Label = "LocalNotificationsSample", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTop)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+        }
+    }
+}
+
